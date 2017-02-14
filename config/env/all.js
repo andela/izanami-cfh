ad364dbf1614ac1778ffbafe709e4d475fdcb8b4
andela-opikuda <oluwadamisi.pikuda@andela.com>
2017-02-14 12:09:32 +0100

    chore (travisCI): replace travis.yml file and correct all.js

--- config/env/all.js
+++ config/env/all.js
@@ -5,8 +5,9 @@ var keys = rootPath + '/keys.txt';
 module.exports = {
 	root: rootPath,
 	port: process.env.PORT || 3000,
-	db: 'mongodb://iampikuda:unlock@ds151289.mlab.com:51289/izanamiteam'
+	db: 'process.env.MONGOHQ_URL'
 };
 
 
+// 'mongodb://iampikuda:unlock@ds151289.mlab.com:51289/izanamiteam'
     // db: process.env.MONGOHQ_URL
