55d6e8d4f26de0ada3bdcdb444dd3b2c2f02d4db
andela-ddiei <delores.diei@andela.com>
2017-02-16 11:42:47 +0100

    chore(grunt-gulp): convert grunt to gulp (resolve merge conflicts) [#139456247]

--- config/env/all.js
+++ config/env/all.js
@@ -3,14 +3,7 @@ var path = require('path'),
 var keys = rootPath + '/keys.txt';
 
 module.exports = {
-<<<<<<< HEAD
     root: rootPath,
     port: process.env.PORT || 3000,
-    db: process.env.MONGOHQ_URL || 'mongodb://localhost:27017/izanami-cfh'
+    db: process.env.MONGOHQ_URL
 };
-=======
-	root: rootPath,
-	port: process.env.PORT || 3000,
-	db: process.env.MONGOHQ_URL
-};
->>>>>>> 5b865fc8a36cd1ad11875092e7be2556196ee751
