e789d36a594f67be3360173733316f7f5427a5a8
Inumidun Amao <inumidun.amao@andela.com>
2017-02-27 10:58:07 +0000

    feature(update) : fix eslint violations

--- config/middlewares/authorization.js
+++ config/middlewares/authorization.js
@@ -1,52 +1,49 @@
-/**
+/*
  * Generic require login routing middleware
  */
-exports.requiresLogin = function(req, res, next) {
-    if (!req.isAuthenticated()) {
-        return res.send(401, 'User is not authorized');
-    }
-    next();
+exports.requiresLogin = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    return res.send(401, 'User is not authorized');
+  }
+  next();
 };
 
 /**
  * User authorizations routing middleware
  */
 exports.user = {
-    hasAuthorization: function(req, res, next) {
-        if (req.profile.id != req.user.id) {
-            return res.send(401, 'User is not authorized');
-        }
-        next();
+  hasAuthorization(req, res, next) {
+    if (req.profile.id !== req.user.id) {
+      return res.send(401, 'User is not authorized');
     }
+    next();
+  }
 };
 
 
 // Change search location of JWT
 const expressJwt = require('express-jwt');
-const authenticate = expressJwt({secret : process.env.SECRET_KEY,
-                                getToken : function fromHeaderOrQueryString(req){
-                                  
-                                  if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-                                    return req.headers.authorization.split(' ')[1];
-                                  } else if (req.cookies && req.cookies.token) {
-                                      
-                                    return req.cookies.token;
-                                  }
-                                  return null;
-                                }});
-  
+
+const authenticate = expressJwt({ secret: process.env.SECRET_KEY,
+  getToken: function fromHeaderOrQueryString(req) {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+      return req.headers.authorization.split(' ')[1];
+    } else if (req.cookies && req.cookies.token) {
+      return req.cookies.token;
+    }
+    return null;
+  } });
+
 const compose = require('composable-middleware');
 
 
-
-//Custom middleware to check if user has authorization and decode token.
-exports.hasAuth = () => {
-  return compose()
+// Custom middleware to check if user has authorization and decode token.
+exports.hasAuth = () => compose()
   .use((req, res, next) => {
     authenticate(req, res, next);
   })
   .use((err, req, res, next) => {
-    if(err) {
+    if (err) {
       if (req.route.path === '/') {
         next();
       } else {
@@ -54,7 +51,6 @@ exports.hasAuth = () => {
       }
     }
   });
-}
 
 /**
  * Article authorizations routing middleware
