cbe63f87a9e32657385609f1e55366964e17a513
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-03-09 11:58:19 +0100

    feature(effective-ui): resolve indentation issues [139456257]

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -60,15 +60,15 @@ exports.checkAvatar = (req, res) => {
     User.findOne({
       _id: req.user._id
     })
-            .exec((err, user) => {
-              if (user.avatar !== undefined) {
-                res.redirect('/#!/');
-              } else {
-                res.redirect('/#!/choose-avatar');
-              }
-            });
+    .exec((err, user) => {
+      if (user.avatar !== undefined) {
+        res.redirect('/#!/');
+      } else {
+        res.redirect('/#!/choose-avatar');
+      }
+    });
   } else {
-        // If user doesn't even exist, redirect to /
+    // If user doesn't even exist, redirect to /
     res.redirect('/');
   }
 };
@@ -83,7 +83,7 @@ exports.create = (req, res, next) => {
     }).exec((err, existingUser) => {
       if (!existingUser) {
         const user = new User(req.body);
-                // Switch the user's avatar index to an actual avatar url
+        // Switch the user's avatar index to an actual avatar url
         user.avatar = avatars[user.avatar];
         user.provider = 'local';
         user.save((err) => {
@@ -111,41 +111,41 @@ exports.create = (req, res, next) => {
  * Assign avatar to user
  */
 exports.avatars = (req, res) => {
-    // Update the current user's profile to include the avatar choice they've made
+  // Update the current user's profile to include the avatar choice they've made
   if (req.user && req.user._id && req.body.avatar !== undefined &&
-        /\d/.test(req.body.avatar) && avatars[req.body.avatar]) {
+    /\d/.test(req.body.avatar) && avatars[req.body.avatar]) {
     User.findOne({
       _id: req.user._id
     })
-            .exec((err, user) => {
-              user.avatar = avatars[req.body.avatar];
-              user.save();
-            });
+    .exec((err, user) => {
+      user.avatar = avatars[req.body.avatar];
+      user.save();
+    });
   }
   return res.redirect('/#!/app');
 };
 
 exports.addDonation = (req, res) => {
   if (req.body && req.user && req.user._id) {
-        // Verify that the object contains crowdrise data
+    // Verify that the object contains crowdrise data
     if (req.body.amount && req.body.crowdrise_donation_id && req.body.donor_name) {
       User.findOne({
         _id: req.user._id
       })
-                .exec((err, user) => {
-                    // Confirm that this object hasn't already been entered
-                  let duplicate = false;
-                  for (let i = 0; i < user.donations.length; i++) {
-                    if (user.donations[i].crowdrise_donation_id === req.body.crowdrise_donation_id) {
-                      duplicate = true;
-                    }
-                  }
-                  if (!duplicate) {
-                    user.donations.push(req.body);
-                    user.premium = 1;
-                    user.save();
-                  }
-                });
+      .exec((err, user) => {
+          // Confirm that this object hasn't already been entered
+        let duplicate = false;
+        for (let i = 0; i < user.donations.length; i++) {
+          if (user.donations[i].crowdrise_donation_id === req.body.crowdrise_donation_id) {
+            duplicate = true;
+          }
+        }
+        if (!duplicate) {
+          user.donations.push(req.body);
+          user.premium = 1;
+          user.save();
+        }
+      });
     }
   }
   res.send();
@@ -175,15 +175,15 @@ exports.me = (req, res) => {
  */
 exports.user = (req, res, next, id) => {
   User
-        .findOne({
-          _id: id
-        })
-        .exec((err, user) => {
-          if (err) return next(err);
-          if (!user) return next(new Error(`Failed to load User ${id}`));
-          req.profile = user;
-          next();
-        });
+    .findOne({
+      _id: id
+    })
+    .exec((err, user) => {
+      if (err) return next(err);
+      if (!user) return next(new Error(`Failed to load User ${id}`));
+      req.profile = user;
+      next();
+    });
 };
 
 
